492b3c3645ca4e4a47d8b4e7cc1be6a7caeb30bc
Elippsis007 <michaeldrmm@gmail.com>
2022-01-16 21:05:37 +0000

    added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+# Inheriting from models.Model
+# Name is a characther field that represents the programmatic name (eg. bed_bath)
+# The length of the programmatic name will be a max length of 254 characters long
+# A friendly name (eg. Bed & Bath) which will make that name a little more friendly looking for the Frontend
+# null and blank = True means that the friendly name is optional
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    # Takes in the category model (above) itself.
+    # And simply returns self.name
+    # Basically this returns the Category Name
+    def __str__(self):
+        return self.name
+
+    # A quick model method same as the string method (above) but will return "friendly_name" if I want it.
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# The first field is a "foreign key" to the category model.
+# It's being allowed null in the database and blank in forms
+# and if a category is deleted I'll set any products that use it to have null for this field
+# rather than deleting the product
+# Each product has a SKU, NAME, DESCRIPTION
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    # Will return the products name
