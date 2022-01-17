d1f770e90847859c4d6f45dc1f6a87bf35a3d75b
Elippsis007 <michaeldrmm@gmail.com>
2022-01-17 21:20:19 +0000

    Added products page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    # Return all products from the database using: product.objects.all()
+    products = Product.objects.all()
+
+    # Context so that the products will be available in the template
+    context = {
+        'products': products,
+    }
+
+    # This is retruning the product webpage
+                # Context is for when I need to send some things back to the template
+    return render(request, 'products/products.html', context)
